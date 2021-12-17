764952b0039d0427802dfa8a4130d9704cd2a398
Will Hodges <hodgeswt@users.noreply.github.com>
2021-12-17 17:43:06 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
     return (
         <div>
-            <h1>Typing Metronome</h1>
+            <h1 style={{fontFamily: "Courier"}}>Typing Metronome</h1>
         </div>
     )
 }
